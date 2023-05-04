f07fd8afb76dbd4206282da562640443c1d01c6e
Cristian Cavanzo <cristian.frontendev@gmail.com>
2023-05-04 08:19:06 -0500

    [Add]: component card to projects

--- src/layout/Projects/components/Card.tsx
+++ src/layout/Projects/components/Card.tsx
@@ -1,12 +1,57 @@
 import React from 'react';
 import styled from 'styled-components';
-const CardComponen = styled.div`
+const CardComponent = styled.div<{ image: string }>`
+	display: inline-flex;
+	width: 100%;
 	height: 700px;
 	padding: 40px 12px;
+	.cardComponent_container {
+		position: relative;
+		width: 100%;
+		background: url(${(props) => props.image});
+		background-size: cover;
+		display: flex;
+		align-items: flex-end;
+		padding: 40px 16px;
+	}
+	.cardComponent_gradient {
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		z-index: 1;
+		background: linear-gradient(
+			0deg,
+			rgba(0, 0, 0, 85%) 0%,
+			rgb(0 0 0 / 20%) 30%,
+			rgb(0 0 0 / 0%) 100%
+		);
+	}
+	.cardComponent_container--text {
+		z-index: 1;
+		font-size: 32px;
+		color: #fff;
+	}
 `;
 
-const Card = ({}) => {
-	return <div></div>;
+interface Props {
+	children: React.ReactNode;
+	image: {
+		src: string;
+		alt: string;
+	};
+}
+
+const Card = ({ children, image }: Props) => {
+	return (
+		<CardComponent image={image.src}>
+			<div className="cardComponent_container">
+				<div className="cardComponent_gradient"></div>
+				<div className="cardComponent_container--text text-bold text-uppercase">{children}</div>
+			</div>
+		</CardComponent>
+	);
 };
 
 export { Card };
--- src/layout/Projects/index.tsx
+++ src/layout/Projects/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
 import Lines from '@img/lines_background.png';
+import { Card } from './components/Card';
+import Project1 from '@img/projects1.png';
+import Project2 from '@img/projects2.png';
+import Project3 from '@img/projects3.png';
 
 const ProjectsComponent = styled.section`
 	background: #7e7f76;
 	padding: 80px 40px;
 	position: relative;
+	.projects_container {
+		display: grid;
+		grid-template-columns: 2fr 1fr 1fr;
+	}
 	.projects_lines {
 		top: 0;
 		left: 0;
@@ -18,10 +26,39 @@ const ProjectsComponent = styled.section`
 `;
 
 const Projects = () => {
+	const CardInfo = [
+		{
+			image: {
+				src: Project1.src,
+				alt: 'Imagen de los proyectos',
+			},
+			text: 'Giorgia Gucci',
+		},
+		{
+			image: {
+				src: Project2.src,
+				alt: 'Imagen de los proyectos',
+			},
+			text: 'Giorgia Gucci',
+		},
+		{
+			image: {
+				src: Project3.src,
+				alt: 'Imagen de los proyectos',
+			},
+			text: 'Giorgia Gucci',
+		},
+	];
 	return (
 		<ProjectsComponent>
 			<div className="projects_lines"></div>
-			<div className="projects_container"></div>
+			<div className="projects_container">
+				{CardInfo.map((card, key) => (
+					<Card image={card.image} key={`projects_${key}`}>
+						{card.text}
+					</Card>
+				))}
+			</div>
 		</ProjectsComponent>
 	);
 };
